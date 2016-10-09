9950ebaaa6978b69b4272bf00211f98d1de3f141
tg <tgdevjs@gmail.com>
2016-10-08 22:51:39 -0400

    Added ListView.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,85 @@
 import React, {Comonent} from 'react';
-import {View,
+import {
+  View,
   Text,
-  StyleSheet} from 'react-native';
+  StyleSheet,
+  ListView,
+  } from 'react-native';
 import Geocoder from 'react-native-geocoder';
 
-var eventbriteAPI =  require('./api/EventbriteAPI')
+const eventbriteAPI =  require('./api/EventbriteAPI')
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1!==r2});
+
 
 module.exports = React.createClass({
+  getInitialState() {
+    return ({
+      dataSource: ds.cloneWithRows([
+        {
+          name: {
+            text: 'Event 1'
+          },
+          url: 'www.eventone.com'
+        },
+        {
+          name: {
+            text: 'Event 1'
+          },
+          url: 'www.eventone.com'
+        },
+        {
+          name: {
+            text: 'Event 1'
+          },
+          url: 'www.eventone.com'
+        },
+        {
+          name: {
+            text: 'Event 1'
+          },
+          url: 'www.eventone.com'
+        },
+        {
+          name: {
+            text: 'Event 1'
+          },
+          url: 'www.eventone.com'
+        },
+        {
+          name: {
+            text: 'Event 1'
+          },
+          url: 'www.eventone.com'
+        },
+      ]),
+    });
+  },
   componentDidMount() {
     eventbriteAPI.searchEvents('hackathon', 'San Francisco');
+  },
+  renderRow(rowData) {
+    return (
+      <View>
+        <Text>
+          {rowData.name.text}
+        </Text>
+        <Text>
+          {rowData.url}
+        </Text>
+      </View>
+    )
   },
   render() {
     return(
       <View style={styles.container}>
-        <Text>
+        <Text style={styles.title}>
           Main
         </Text>
+        <ListView
+          style={styles.list}
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => this.renderRow(rowData)}
+        />
       </View>
     );
   }
@@ -26,5 +90,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    flex: 1,
+    marginTop: 40,
+  },
+  list: {
+    flex: 1,
   }
 });
